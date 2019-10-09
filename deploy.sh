5906f90ebf7e5571dead3a965375c1fac82b5a07
Huy <tqhuy1996.developer@gmail.com>
2019-10-09 10:26:07 +0700

    gitignore

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-function build() {
-    docker login -u "tqhuy1996developer" -p "34f93368-d658-44d7-89eb-cd0519f90b03"
-    docker build . -t "tqhuy1996developer/istock"
-    docker push "tqhuy1996developer/istock"
-}
-
-build
